04024a9bf22dcfc60988d37d216189239f3e05bb
Christian Pirhalla <chrispir@udel.edu>
2021-01-03 23:57:00 -0500

    initial upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Jerry Seinfeld' },
+  { id: 12, name: 'Rodney Dangerfield' },
+  { id: 13, name: 'Fred Durst' },
+  { id: 14, name: 'Jon Davis' },
+  { id: 15, name: 'Wario' },
+  { id: 16, name: 'Mario' },
+  { id: 17, name: 'Rick Sanchez' },
+  { id: 18, name: 'Morty Smith' },
+  { id: 19, name: 'Frylock' },
+  { id: 20, name: 'Meatwad' }
+];
